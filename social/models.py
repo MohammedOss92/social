99e7ce20fa1728f06d22b992240a49df12496fba
MohammedOss <abdallahomar1992@gmail.com>
2023-05-10 20:05:21 +0300

    aa

--- social/models.py
+++ social/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    body = models.TextField()
+    created_on = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+# class Comment(models.Model):
+#     comment = models.TextField()
+#     created_on = models.DateTimeField(default=timezone.now)
+#     post = models.ForeignKey('Post', on_delete=models.CASCADE)
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
--- socialNetwork/settings.py
+++ socialNetwork/settings.py
@@ -147,7 +147,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+LOGIN_REDIRECT_URL = 'post-list'
+ACCOUNT_EMAIL_REQUIRED = True
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
